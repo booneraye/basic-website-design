d13034d13036e0ea5321156d58bc91f247c5a96e
booneraye <booneraye@gmail.com>
2019-06-24 14:54:51 +0800

    Day 2 updates

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ var b;
 
 for (b = 0; b < coll.length; b++) {
   coll[b].addEventListener("click", function() {
-    this.classList.toggle("active");
+    this.classList.toggle("collapsible-active");
     var content = this.nextElementSibling;
     if (content.style.display === "block") {
       content.style.display = "none";
@@ -24,4 +24,46 @@ for (b = 0; b < coll.length; b++) {
       content.style.display = "block";
     }
   });
+}
+
+
+// Tyewriter Effect
+var c = 0;
+var txt = 'COMING BACK SOON!';
+var speed = 100;
+
+function typeWriter() {
+  if (c < txt.length) {
+    document.getElementById("typewriter-effect").innerHTML += txt.charAt(c);
+    c++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+//Slideshow effects
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(d) {
+  showSlides(slideIndex += d);
+}
+
+function currentSlide(d) {
+  showSlides(slideIndex = d);
+}
+
+function showSlides(d) {
+  var e;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (d > slides.length) {slideIndex = 1}    
+  if (d < 1) {slideIndex = slides.length}
+  for (e = 0; e < slides.length; e++) {
+      slides[e].style.display = "none";  
+  }
+  for (e = 0; e < dots.length; e++) {
+      dots[e].className = dots[e].className.replace(" slideshow-active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " slideshow-active";
 }
